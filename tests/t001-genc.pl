d53ec44cb039d92f7f30aac69bcfcb7ef7456769
Greg Banks <gnb@fmeh.org>
2011-10-17 16:33:39 +1100

    Add t001 with lots of little test functions
    generated on the fly by a Perl script.  Each test function does
    nothing except sleep for a random time, with a message to stderr
    before and after.

---
+++ tests/t001-genc.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $N = 50;
+
+print <<EOF;
+#include <syslog.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include "u4c.h"
+
+EOF
+
+foreach my $i (1..$N)
+{
+    my $d = int(1000000.0 * (0.25 + rand(0.75)));
+
+    print <<EOF;
+static void
+test_$i(void)
+{
+    fprintf(stderr, "%s test_$i begins\\n", u4c_reltimestamp()); fflush(stderr);
+    usleep($d);
+    fprintf(stderr, "%s test_$i ends\\n", u4c_reltimestamp()); fflush(stderr);
+}
+
+EOF
+}
+
+print <<EOF;
+int
+main(int argc, char **argv)
+{
+    int ec = 0;
+    u4c_globalstate_t *state = u4c_init();
+    ec = u4c_run_tests(state);
+    u4c_done(state);
+    exit(ec);
+}
+EOF
