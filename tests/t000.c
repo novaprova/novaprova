2b6311d1a502610d282403e530782dda26ba7968
Greg Banks <gnb@fmeh.org>
2012-03-29 17:05:48 +1100

    t000 takes commandline arguments
    allowing specific tests to be run from the commandline.  Well, it
    would do if plan_t worked as designed.

--- tests/t000.c
+++ tests/t000.c
@@ -60,8 +60,15 @@ int
 main(int argc, char **argv)
 {
     int ec = 0;
+    u4c_plan_t *plan = 0;
+    if (argc > 1)
+    {
+	plan = u4c_plan_new();
+	u4c_plan_add_specs(plan, argc-1, (const char **)argv+1);
+    }
     u4c_runner_t *runner = u4c_init();
-    ec = u4c_run_tests(runner, 0);
+    ec = u4c_run_tests(runner, plan);
+    if (plan) u4c_plan_delete(plan);
     u4c_done(runner);
     exit(ec);
 }
