7297ab2dea5a5c0c48783591c608ff730a809f87
Greg Banks <gnb@fmeh.org>
2012-05-31 10:07:58 +1000

    Convert tintercept to metafw

--- tests/tintercept.cxx
+++ tests/tintercept.cxx
@@ -1,6 +1,8 @@
 #include "np/spiegel/spiegel.hxx"
 #include "np/spiegel/dwarf/state.hxx"
 #include <libintl.h>
+#include "fw.h"
+
 using namespace std;
 using namespace np::util;
 
@@ -45,14 +47,14 @@ the_function(int x, int y)
 {
     int i;
 
-    printf("Start of the_function, x=%d y=%d\n", x, y);
+    if (is_verbose()) printf("Start of the_function, x=%d y=%d\n", x, y);
     the_function_count++;
     for (i = 0 ; i < x ; i++)
     {
 	y *= 5;
 	y--;
     }
-    printf("End of the_function, returning %d\n", y);
+    if (is_verbose()) printf("End of the_function, returning %d\n", y);
     return y;
 }
 
@@ -63,14 +65,14 @@ another_function(int x, int y)
 {
     int i;
 
-    printf("Start of another_function, x=%d y=%d\n", x, y);
+    if (is_verbose()) printf("Start of another_function, x=%d y=%d\n", x, y);
     another_function_count++;
     for (i = 0 ; i < x ; i++)
     {
 	y *= 2;
 	y++;
     }
-    printf("End of another_function, returning %d\n", y);
+    if (is_verbose()) printf("End of another_function, returning %d\n", y);
     return y;
 }
 
@@ -107,15 +109,15 @@ intercept_tester_t::before(np::spiegel::call_t &call)
     x = call.get_arg(0);
     y = call.get_arg(1);
     before_count++;
-    printf("BEFORE x=%d y=%d\n", x, y);
+    if (is_verbose()) printf("BEFORE x=%d y=%d\n", x, y);
     if (test_skip)
     {
-	printf("SKIPPING r=%d\n", r);
+	if (is_verbose()) printf("SKIPPING r=%d\n", r);
 	call.skip(r);
     }
     if (test_redirect)
     {
-	printf("REDIRECTING\n");
+	if (is_verbose()) printf("REDIRECTING\n");
 	call.redirect((np::spiegel::addr_t)&another_function);
     }
 }
@@ -125,7 +127,7 @@ intercept_tester_t::after(np::spiegel::call_t &call)
 {
     r = call.get_retval();
     after_count++;
-    printf("AFTER, returning %d\n", r);
+    if (is_verbose()) printf("AFTER, returning %d\n", r);
 }
 
 int moe(int x, int y)
@@ -173,12 +175,12 @@ public:
 	int i;
 	for (i = 0 ; i < 12 ; i++)
 	    x[i] = call.get_arg(i);
-	printf("BEFORE\n");
+	if (is_verbose()) printf("BEFORE\n");
     }
     void after(np::spiegel::call_t &call)
     {
 	r = call.get_retval();
-	printf("AFTER, returning %d\n", r);
+	if (is_verbose()) printf("AFTER, returning %d\n", r);
     }
 };
 
@@ -198,13 +200,13 @@ public:
 
     void before(np::spiegel::call_t &call)
     {
-	printf("BEFORE, arg0 \"%s\"\n", (const char *)call.get_arg(0));
+	if (is_verbose()) printf("BEFORE, arg0 \"%s\"\n", (const char *)call.get_arg(0));
 	before_count++;
     }
     void after(np::spiegel::call_t &call)
     {
 	const char *s = (const char *)call.get_retval();
-	printf("AFTER, returning \"%s\"\n", s);
+	if (is_verbose()) printf("AFTER, returning \"%s\"\n", s);
 	after_count++;
 	call.set_retval((unsigned long)"world");
     }
@@ -225,144 +227,162 @@ main(int argc, char **argv __attribute__((unused)))
     if (!state.add_self())
 	return 1;
 
-    intercept_tester_t *it = new intercept_tester_t();
+    intercept_tester_t *it;
+    int r;
+
+    BEGIN("basic");
+    it = new intercept_tester_t();
     it->install();
 
-    assert(it->x == 0);
-    assert(it->y == 0);
-    assert(it->r == 0);
-    assert(it->before_count == 0);
-    assert(it->after_count == 0);
-    assert(the_function_count == 0);
-    assert(another_function_count == 0);
+    CHECK(it->x == 0);
+    CHECK(it->y == 0);
+    CHECK(it->r == 0);
+    CHECK(it->before_count == 0);
+    CHECK(it->after_count == 0);
+    CHECK(the_function_count == 0);
+    CHECK(another_function_count == 0);
 
-    int r = larry(0, 39);
+    r = larry(0, 39);
 
-    assert(r == 5216);
-    assert(it->x == 3);
-    assert(it->y == 42);
-    assert(it->r == 5219);
-    assert(it->before_count == 1);
-    assert(it->after_count == 1);
-    assert(the_function_count == 1);
-    assert(another_function_count == 0);
+    CHECK(r == 5216);
+    CHECK(it->x == 3);
+    CHECK(it->y == 42);
+    CHECK(it->r == 5219);
+    CHECK(it->before_count == 1);
+    CHECK(it->after_count == 1);
+    CHECK(the_function_count == 1);
+    CHECK(another_function_count == 0);
 
     r = larry(4, 24);
 
-    assert(r == 2089841);
-    assert(it->x == 7);
-    assert(it->y == 27);
-    assert(it->r == 2089844);
-    assert(it->before_count == 2);
-    assert(it->after_count == 2);
-    assert(the_function_count == 2);
-    assert(another_function_count == 0);
+    CHECK(r == 2089841);
+    CHECK(it->x == 7);
+    CHECK(it->y == 27);
+    CHECK(it->r == 2089844);
+    CHECK(it->before_count == 2);
+    CHECK(it->after_count == 2);
+    CHECK(the_function_count == 2);
+    CHECK(another_function_count == 0);
+    END;
 
+    BEGIN("skip");
     it->test_skip = true;
     it->r = 327;
     r = larry(1, 53);
     it->test_skip = false;
 
-    assert(r == 324);
-    assert(it->x == 4);
-    assert(it->y == 56);
-    assert(it->r == 327);
-    assert(it->before_count == 3);
-    assert(it->after_count == 2);
-    assert(the_function_count == 2);
-    assert(another_function_count == 0);
+    CHECK(r == 324);
+    CHECK(it->x == 4);
+    CHECK(it->y == 56);
+    CHECK(it->r == 327);
+    CHECK(it->before_count == 3);
+    CHECK(it->after_count == 2);
+    CHECK(the_function_count == 2);
+    CHECK(another_function_count == 0);
+    END;
 
+    BEGIN("redirect");
     it->test_redirect = true;
     r = larry(0, 39);
     it->test_redirect = false;
 
-    assert(r == 340);
-    assert(it->x == 3);
-    assert(it->y == 42);
-    assert(it->r == 343);
-    assert(it->before_count == 4);
-    assert(it->after_count == 3);
-    assert(the_function_count == 2);
-    assert(another_function_count == 1);
+    CHECK(r == 340);
+    CHECK(it->x == 3);
+    CHECK(it->y == 42);
+    CHECK(it->r == 343);
+    CHECK(it->before_count == 4);
+    CHECK(it->after_count == 3);
+    CHECK(the_function_count == 2);
+    CHECK(another_function_count == 1);
+    END;
 
-    intercept_tester_t *it2 = new intercept_tester_t();
+    intercept_tester_t *it2;
+    BEGIN("two intercepts");
+    it2 = new intercept_tester_t();
     it2->install();
 
     r = larry(5, 23);
 
-    assert(r == 10058591);
-    assert(it->x == 8);
-    assert(it->y == 26);
-    assert(it->r == 10058594);
-    assert(it->before_count == 5);
-    assert(it->after_count == 4);
-    assert(it2->x == 8);
-    assert(it2->y == 26);
-    assert(it2->r == 10058594);
-    assert(it2->before_count == 1);
-    assert(it2->after_count == 1);
-    assert(the_function_count == 3);
-    assert(another_function_count == 1);
+    CHECK(r == 10058591);
+    CHECK(it->x == 8);
+    CHECK(it->y == 26);
+    CHECK(it->r == 10058594);
+    CHECK(it->before_count == 5);
+    CHECK(it->after_count == 4);
+    CHECK(it2->x == 8);
+    CHECK(it2->y == 26);
+    CHECK(it2->r == 10058594);
+    CHECK(it2->before_count == 1);
+    CHECK(it2->after_count == 1);
+    CHECK(the_function_count == 3);
+    CHECK(another_function_count == 1);
+    END;
 
+    BEGIN("uninstall");
     it2->uninstall();
     delete it2;
 
     r = larry(7, 11);
 
-    assert(r == 134277341);
-    assert(it->x == 10);
-    assert(it->y == 14);
-    assert(it->r == 134277344);
-    assert(it->before_count == 6);
-    assert(it->after_count == 5);
-    assert(the_function_count == 4);
-    assert(another_function_count == 1);
+    CHECK(r == 134277341);
+    CHECK(it->x == 10);
+    CHECK(it->y == 14);
+    CHECK(it->r == 134277344);
+    CHECK(it->before_count == 6);
+    CHECK(it->after_count == 5);
+    CHECK(the_function_count == 4);
+    CHECK(another_function_count == 1);
+    END;
 
 //     fprintf(stderr, "About to SEGV\n");
 //     *(char *)0 = 0;
 //     fprintf(stderr, "Done doing SEGV\n");
 
+    BEGIN("uninstall last");
     it->uninstall();
     delete it;
 
     r = larry(3, 7);
 
-    assert(r == 152341);
-    assert(the_function_count == 5);
-    assert(another_function_count == 1);
+    CHECK(r == 152341);
+    CHECK(the_function_count == 5);
+    CHECK(another_function_count == 1);
+    END;
 
 
-    // Test a larger stack frame
+    BEGIN("larger stack frame");
     wide_intercept_tester_t *it3 = new wide_intercept_tester_t;
     it3->install();
     r = wide_call(1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144);
-    assert(it3->x[0] == 1);
-    assert(it3->x[1] == 4);
-    assert(it3->x[2] == 9);
-    assert(it3->x[3] == 16);
-    assert(it3->x[4] == 25);
-    assert(it3->x[5] == 36);
-    assert(it3->x[6] == 49);
-    assert(it3->x[7] == 64);
-    assert(it3->x[8] == 81);
-    assert(it3->x[9] == 100);
-    assert(it3->x[10] == 121);
-    assert(it3->x[11] == 144);
-    assert(it3->r == 650);
+    CHECK(it3->x[0] == 1);
+    CHECK(it3->x[1] == 4);
+    CHECK(it3->x[2] == 9);
+    CHECK(it3->x[3] == 16);
+    CHECK(it3->x[4] == 25);
+    CHECK(it3->x[5] == 36);
+    CHECK(it3->x[6] == 49);
+    CHECK(it3->x[7] == 64);
+    CHECK(it3->x[8] == 81);
+    CHECK(it3->x[9] == 100);
+    CHECK(it3->x[10] == 121);
+    CHECK(it3->x[11] == 144);
+    CHECK(it3->r == 650);
     it3->uninstall();
     delete it3;
+    END;
 
+    BEGIN("plt");
     plt_intercept_tester_t *it4 = new plt_intercept_tester_t();
     it4->install();
-    assert(it4->before_count == 0);
-    assert(it4->after_count == 0);
+    CHECK(it4->before_count == 0);
+    CHECK(it4->after_count == 0);
     const char *s = gettext("hello");
-    assert(!strcmp(s, "world"));
-    assert(it4->before_count == 1);
-    assert(it4->after_count == 1);
+    CHECK(!strcmp(s, "world"));
+    CHECK(it4->before_count == 1);
+    CHECK(it4->after_count == 1);
     it4->uninstall();
+    END;
 
-    printf("PASS\n");
     return 0;
 }
 
