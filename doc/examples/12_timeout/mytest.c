e4c399eabcce6d8d0fca3ebf72e4cbf8e31f0b49
Greg Banks <gnb@fmeh.org>
2013-01-27 00:29:05 +1100

    Add example 12_timeout

---
+++ doc/examples/12_timeout/mytest.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2011-2013 Gregory Banks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <np.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void test_slow(void)
+{
+    int i;
+    fprintf(stderr, "Test runs for 100 seconds\n");
+    for (i = 0 ; i < 100 ; i += 10) {
+	fprintf(stderr, "Have been running for %d sec\n", i);
+	sleep(10);
+    }
+    fprintf(stderr, "Test ends after %d sec\n", i);
+}
+
