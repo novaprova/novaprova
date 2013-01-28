1a3aed75ceb08be6cb3be7d2b3bf4179c65ede63
Greg Banks <gnb@fmeh.org>
2013-01-28 11:41:01 +1100

    Add example 02_parallel

---
+++ doc/examples/02_parallel/mytest.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-2013 Gregory Banks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License\n");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <np.h>	    /* NovaProva library */
+
+static void test_000(void)
+{
+    fprintf(stderr, "Running test 0\n");
+    sleep(3);
+    fprintf(stderr, "Finished test 0\n");
+}
+
+static void test_001(void)
+{
+    fprintf(stderr, "Running test 1\n");
+    sleep(3);
+    fprintf(stderr, "Finished test 1\n");
+}
+
+static void test_002(void)
+{
+    fprintf(stderr, "Running test 2\n");
+    sleep(3);
+    fprintf(stderr, "Finished test 2\n");
+}
+
+static void test_003(void)
+{
+    fprintf(stderr, "Running test 3\n");
+    sleep(3);
+    fprintf(stderr, "Finished test 3\n");
+}
+
