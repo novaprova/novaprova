552ff4119be28e5e58913702bc90f2db269b21a1
Greg Banks <gnb@fmeh.org>
2014-09-01 16:36:09 -0700

    All the dummy platform-specific functions whine at runtime

--- np/spiegel/platform/darwin.cxx
+++ np/spiegel/platform/darwin.cxx
@@ -35,58 +35,71 @@ using namespace std;
 
 
 bool get_argv(int *argcp, char ***argvp) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     return false;
 }
 
 char *self_exe() {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     return NULL;
 }
 
 vector<linkobj_t> get_linkobjs() {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     vector<linkobj_t>* res = new vector<linkobj_t>();
     return *res;
 }
 
 np::spiegel::addr_t normalise_address(np::spiegel::addr_t addr) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     return addr;
 }
 
 void add_plt(const np::spiegel::mapping_t &m) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
 }
 
 int text_map_writable(addr_t addr, size_t len) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     return -1;
 }
 
 int text_restore(addr_t addr, size_t len) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     return -1;
 }
 
 vector<np::spiegel::addr_t> get_stacktrace() {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     vector<np::spiegel::addr_t>* res = new vector<np::spiegel::addr_t>();
     return *res;
 }
 
 bool is_running_under_debugger() {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     return false;
 }
 
 vector<string> get_file_descriptors() {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     vector<string>* res = new vector<string>();
     return *res;
 }
 
 int install_intercept(np::spiegel::addr_t addr, intstate_t &state, std::string &err) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     err = "Not yet implemented";
     return -1;
 }
 
 int uninstall_intercept(np::spiegel::addr_t addr, intstate_t &state, std::string &err) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     err = "Not yet implemented";
     return -1;
 }
 
 int clock_gettime(clockid_t clk_id, struct timespec *res) {
+    fprintf(stderr, "TODO: %s not implemented for this platform\n", __FUNCTION__);
     errno = ENOSYS;
     return -1;
 }
