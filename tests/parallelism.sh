90c43a53a4541aae9cda8ead719957962eb02079
Greg Banks <gnb@fmeh.org>
2014-10-05 21:23:29 -0700

    Don't run parallelism tests on low CPU count machines

---
+++ tests/parallelism.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#  Copyright 2011-2014 Gregory Banks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+os=$(uname -s)
+case "$os" in
+Linux)
+    ncpus=$(grep '^processor' /proc/cpuinfo | wc -l)
+    ;;
+Darwin)
+    ncpus=$(sysctl hw.ncpu)
+    ;;
+*)
+    echo "$0: cannot determine available parallelism on this platform" 1>&2
+    ncpus=1
+    ;;
+esac
+i=1
+while [ $i -le $ncpus ] ; do
+    echo -n "$i "
+    i=$[ i * 2 ]
+done
+echo ""
+
