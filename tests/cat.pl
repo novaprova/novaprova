808e9e6f1c8cdcee2f06abf2d90813eefa3cc48f
Greg Banks <gnb@fmeh.org>
2014-10-29 00:19:40 -0700

    Elide empty compile_units from tdumpdstr et al
    This makes tests more reliable across platforms.

--- tests/cat.pl
+++ tests/cat.pl
@@ -3,7 +3,30 @@
 use strict;
 use warnings;
 
+my $s = 0;
+my $n = 0;
 while (<STDIN>)
 {
+    if (m/compile_unit {/)
+    {
+	$s = 1;
+	next;
+    }
+    if ($s && m/} compile_unit/)
+    {
+	if ($n > 0)
+	{
+	    # elide empty compile unit
+	    $n = 0;
+	    print;
+	}
+	$s = 0;
+	next;
+    }
+    if ($s && $n == 0)
+    {
+	print "compile_unit {\n";
+    }
+    $n += $s;
     print;
 }
