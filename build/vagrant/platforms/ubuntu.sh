aeb106934125b58a50c6d839d5d8f1ab32fc3ec1
Greg Banks <gnb@fmeh.org>
2015-07-08 20:38:16 -0700

    Add a Ubuntu 14.04 + gcc 4.9 platform
    using the Erubis templating feature to combine the
    setup script with the plain Ubuntu setup script.

--- build/vagrant/platforms/ubuntu.sh
+++ build/vagrant/platforms/ubuntu.sh
@@ -1,8 +1,17 @@
 # Setup C development environment and prereqs
+<% if @ppa %>
+add-apt-repository -y ppa:<%= @ppa %>
+<% end %>
 apt-get update
 apt-get install -y \
+    <% if @gcc_version %>gcc-<%= @gcc_version %><% end %> \
+    <% if @gcc_version %>g++-<%= @gcc_version %><% end %> \
     git autoconf automake libxml2-dev libxml2-utils \
     pkg-config valgrind binutils-dev doxygen \
     python-pygments python-markdown zlib1g-dev \
     libiberty-dev python-pip
 pip install breathe Sphinx
+<% if @gcc_version %>
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-<%= @gcc_version %> \
+    60 --slave /usr/bin/g++ g++ /usr/bin/g++-<%= @gcc_version %>
+<% end %>
